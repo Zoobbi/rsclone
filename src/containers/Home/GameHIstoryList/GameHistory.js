61d79972150bfb365061862471e52041e640cf13
Oleg <karibyon@gmail.com>
2021-01-27 13:29:33 +0300

    feat: add cur.league game history

---
+++ src/containers/Home/GameHIstoryList/GameHistory.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import './GameHistory.scss';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import { loadGamesFromDB } from '../../../utils/redux/reducers/games';
+
+class GameHistory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      league_id: undefined,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.league_id !== prevProps.league_id) {
+      this.props.fetchGamesFromDB(this.props.league_id);
+    }
+    console.log(this.props.games, this.props.teams);
+  }
+
+  showGamesList = () => this.props.games.games.map((game) => {
+    const date = new Date(game.date);
+    console.log(date);
+    return (
+      <li key={game._id + Math.random()} className="GameHistoryAll-item">
+        <Link to={`/games/detals?game_id=${game._id}`}>
+          <span className="GameHistoryAll-teams">
+            {game.team_visit_name}
+            {' '}
+            -
+            {' '}
+            {game.team_home_name}
+          </span>
+          <span className="GameHistoryAll-date">{`${date.getDate()} - ${date.getMonth() + 1} - ${date.getFullYear()}`}</span>
+        </Link>
+      </li>
+    );
+  })
+
+  render() {
+    return (
+      <div className="GameHistoryAll">
+        <h2>Game History</h2>
+        <ul className="GameHistory-list">
+          {this.props.games.games ? this.showGamesList() : null}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchGamesFromDB: (leagueID) => {
+    dispatch(loadGamesFromDB(leagueID));
+  },
+});
+
+const mapStateToProps = (state) => ({
+  teams: state.teams,
+  games: state.games,
+  currentLeague: state.currentLeague,
+});
+
+GameHistory.defaultProps = {
+  league_id: undefined,
+  fetchGamesFromDB: 'null',
+  games: [],
+  teams: [],
+};
+
+GameHistory.propTypes = {
+  league_id: propTypes.string,
+  fetchGamesFromDB: propTypes.func,
+  // eslint-disable-next-line react/forbid-prop-types
+  games: propTypes.array,
+  // eslint-disable-next-line react/forbid-prop-types
+  teams: propTypes.array,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameHistory);
